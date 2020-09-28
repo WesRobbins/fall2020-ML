54ab8082faa7d4fd935a9f733cdd948ae3e80ea4
Seth <sethbassetti@gmail.com>
2020-09-28 11:03:02 -0600

    Added mean absolute error and mean square error evaluations

--- proj2/evaluator.py
+++ proj2/evaluator.py
@@ -19,9 +19,11 @@ class Evaluator:
         print(f"1/0 Loss:\t\t\t{one_zero:.2f}")
 
     def regression_evaluation(self, test_set, predicted_values):
-        self.mean_absolute_error(test_set, predicted_values)
-        self.mean_square_error(test_set, predicted_values)
+        MAE = self.mean_absolute_error(test_set, predicted_values)
+        MSE = self.mean_square_error(test_set, predicted_values)
         self.quantile_error(test_set, predicted_values)
+        print(f"Mean Percent Error:\t{MAE:.2f}")
+        print(f"Mean Square Error:\t{MSE:.2f}")
 
     def percent_accuracy(self, test_set, predicted_values):
         """Returns percent accuracy given true and predicted values"""
@@ -48,11 +50,22 @@ class Evaluator:
 
     # regression loss functions
     def mean_absolute_error(self, test_set, predicted_values):
-        # TODO
-        pass
+        """Returns the mean absolute error for a given test set and their predicted values"""
+
+        running_sum = 0
+        for i in range(len(test_set)):
+            running_sum += abs(test_set[i].classification - predicted_values[i])
+
+        running_sum = running_sum / len(test_set)
+        return running_sum
     def mean_square_error(self, test_set, predicted_values):
-        # TODO
-        pass
+        """Returns the mean square error for a given test set and their predicted values"""
+
+        running_sum = 0
+        for i in range(len(test_set)):
+            running_sum += (test_set[i].classification - predicted_values[i])**2
+        running_sum = running_sum / len(test_set)
+        return running_sum
 
     def quantile_error(self, test_set, predicted_values):
         # TODO
